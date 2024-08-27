c6e18bac9e47a0c7e8f7c7665a9d91f2b8aaf890
Christophe Limpalair <christf24@gmail.com>
2024-08-27 13:04:42 -0600

    check for AZ that supports t3.micro

--- cybr-terraform-code-ec2/main.tf
+++ cybr-terraform-code-ec2/main.tf
@@ -5,10 +5,20 @@ resource "aws_vpc" "custom_vpc" {
   tags = var.aws_tagging
 }
 
+// Get available AZs
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  filtered_azs = [for az in data.aws_availability_zones.available.names : az if contains(var.desired_azs, az)]
+}
+
 // Subnet within the VPC
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.custom_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = element(local.filtered_azs, 0)
 
   tags = var.aws_tagging
 }
