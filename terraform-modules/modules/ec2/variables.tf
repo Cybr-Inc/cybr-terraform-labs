d1ce741c8acdc03b3eb42c8b8d0584035107f33f
Christophe Limpalair <christf24@gmail.com>
2024-08-30 12:59:40 -0600

    added terraform-modules end state source code for modules lesson

---
+++ terraform-modules/modules/ec2/variables.tf
@@ -0,0 +1,29 @@
+variable "instance_type" {
+  type        = string
+  description = "Select the instance type. Options 't2.micro' or 't3.micro'"
+
+  validation {
+    condition     = (var.instance_type == "t2.micro" || var.instance_type == "t3.micro")
+    error_message = "The instance_type must be 't2.micro' or 't3.micro'"
+  }
+}
+
+variable "tagging" {
+  type        = map(string)
+  description = "Add 'BusinessUnit' and other tags"
+
+  validation {
+    condition     = contains(keys(var.tagging), "BusinessUnit")
+    error_message = "At least one tag must be called 'BusinessUnit'"
+  }
+}
+
+variable "image_os" {
+  type        = string
+  description = "Select the image OS. Options 'ubuntu' or 'amazon linux'"
+
+  validation {
+    condition     = (var.image_os == "ubuntu" || var.image_os == "amazon linux")
+    error_message = "The image_os must be 'ubuntu' or 'amazon linux'"
+  }
+}
